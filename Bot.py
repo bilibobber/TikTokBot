307d47878e11c3bd143bc66086385fac72fbc664
Кровавый ангел смерти <QPopaPro@gmail.com>
2025-07-26 04:16:25 +0300

    Last commit contained error. In this commit I fixed error and some changed DEBUG logic.

--- Bot.py
+++ Bot.py
@@ -17,7 +17,7 @@ dp = Dispatcher()
 
 
 async def main() -> None:
-    bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode="HTML", file_upload_limit=100 * 1024 * 1024))
+    bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp.include_router(gpt_router)
     dp.include_router(upload_router)
     await dp.start_polling(bot)
@@ -25,33 +25,8 @@ async def main() -> None:
 
 if __name__ == "__main__":
     try:
-        if os.getenv('DEBUG'):
-            logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+        logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
         asyncio.run(main())
     except KeyboardInterrupt:
         print("Exit")
-
-'''
-import yt_dlp
-
-
-def get_video_youtube_url(url):
-    try:
-        ydl_opts = {
-            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
-            'quiet': True,
-            'no_warnings': True,
-            'simulate': True,
-        }
-
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(url, download=False)  # .extract_info - получить прямую ссылку на видео
-            return info['url']
-    except Exception as e:
-        print(f"Ошибка (get_youtube_video_url): {e}")
-        return False
-
-print(get_video_youtube_url('https://www.youtube.com/shorts/yLlzJtSMHwg?feature=share'))
-
-'''
--- app/scripts/youtube_video_downloader.py
+++ app/scripts/youtube_video_downloader.py
@@ -1,10 +1,32 @@
+import os
+
 import yt_dlp
 
-ydl_opts = {
-    'format': 'best[ext=mp4]',
-    'outtmpl': 'temp/temp.mp4',
-    'overwrites': True
-}
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if os.getenv('DEBUG'):
+    ydl_opts = {
+        'format': 'best[ext=mp4]',
+        'outtmpl': 'temp/temp.mp4',
+        'overwrites': True,
+        'quiet': True,
+        'no_warnings': True,
+        'no_call_home': True,
+        'no_color': True,
+        'no_progress': True,
+        'noprogress': True,
+        'logger': None,
+        'progress_hooks': [],
+        'verbose': False
+    }
+else:
+    ydl_opts = {
+        'format': 'best[ext=mp4]',
+        'outtmpl': 'temp/temp.mp4',
+        'overwrites': True
+    }
 
 
 def download_youtube_video(url):
